cf4eba70e4357bef2e6456d9d43692ffa19e7ddb
Luciano <luciano.gp99@gmail.com>
2025-05-08 02:10:04 -0300

    Ajuste CORS

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
@@ -9,6 +10,8 @@ import usuarioRoutes from './routes/usuario';
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
